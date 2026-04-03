a4b3b65d150c45e6ed473dd4a372d3917d124df5
Edgar Salazar <24004827@galileo.edu>
2026-04-02 21:46:58 -0600

    Initialize firebase

--- functional integration/backend/server.js
+++ functional integration/backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const path = require("path");
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "web")));
 
